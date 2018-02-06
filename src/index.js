942da3300c54731e2c4133c0a6e7483ac753fb2b
George Lima <georgelima11@hotmail.com>
2018-02-06 17:20:24 -0300

    Update isAuth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
+import React, { Component } from 'react'
+import { Route, Redirect } from 'react-router-dom'
 
-export default (props) => {
-  const { auth, component: Component, redirect: pathname, ...rest } = props;
+const isFunction = _ => typeof _ === 'function'
+
+export default props => {
+  const { auth, component: Component, redirect: pathname, ...rest } = props
+
+  const isAuth = isFunction(auth) ? auth(props) : auth
 
   return (
     <Route
       {...rest}
-      render={
-        props => auth ?
-          <Component {...props} /> :
-          <Redirect to={{ pathname, state: {from: props.location} }} />
+      render={props =>
+        isAuth ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname, state: { from: props.location } }} />
+        )
       }
     />
-  );
+  )
 }
